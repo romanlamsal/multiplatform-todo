df77b71084c8294d63895a47d16fa6f0666c4acd
Roman Lamsal <roman.lamsal@mercedes-benz.com>
2024-01-12 10:45:49 +0100

    feat(web): add PWA support

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import * as path from "path"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte()],
+    plugins: [svelte(), VitePWA({ registerType: "autoUpdate" })],
     resolve: {
         alias: {
             $types: path.resolve(__dirname, "./src/types"),
